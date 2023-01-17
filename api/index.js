7ccde4e0f64cd41fa3cf7602f90b84059495f229
Ammar Daham <ammar.daham@metropolia.fi>
2023-01-18 00:06:34 +0200

    birdness init

---
+++ api/index.js
@@ -0,0 +1,32 @@
+const { response } = require('express')
+const express = require('express')
+const request = require('request')
+const parser = require('xml2json')
+
+const PORT = process.env.PORT || 3000
+
+const app = express()
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    next();
+})
+
+app.get('/', (req, res) => {
+    request(
+        {url: 'https://assignments.reaktor.com/birdnest/drones'},
+        (error, response, body) => {
+            if(error || response.statusCode !== 200) {
+              return res.status(500).json({type: 'error', message: error.message})
+            }
+
+            let json = parser.toJson(body);
+            console.log("to json -> %s ", json.length)
+        }
+    )
+})
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
+
+
+
